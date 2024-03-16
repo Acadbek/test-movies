b1585ade3a8dc44065b95a24c9889fe2344e07fc
Acadbek <email manzilingiz>
2024-03-16 15:52:58 +0500

    [FEAT]: layouts

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+const HomeView = () => {
+  return (
+    <>
+      <h1 className="text-[32px] font-normal leading-10 tracking-[-0.5px] text-left">
+        Trending
+      </h1>
+    </>
+  )
+}
+
+export default HomeView;
